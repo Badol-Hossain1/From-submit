c2245745641f5ad7efa918fcab204e44197c8c12
Badol-Hossain <badolhossain586@gmail.com>
2023-10-06 22:48:19 -0700

    feat: how to get data fromsubmit

--- src/components/from/From.jsx
+++ src/components/from/From.jsx
@@ -1,11 +1,18 @@
-
-
 const From = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const handleSubmit = (e) => {
+    console.log("🚀 ~ file: From.jsx:3 ~ handleSubmit ~ e:", e.target.name.value);
+    e.preventDefault();
+    console.log("from submit");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default From;
